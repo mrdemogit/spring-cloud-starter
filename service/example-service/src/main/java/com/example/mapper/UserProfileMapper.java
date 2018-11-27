eed870bb0d7b6480f5eedcf5c2a4d819f5459934
Maros Demko <maros.demko.it@gmail.com>
2018-11-27 22:45:56 +0800

    [SERVICE] Dto layer

--- service/example-service/src/main/java/com/example/mapper/UserProfileMapper.java
+++ service/example-service/src/main/java/com/example/mapper/UserProfileMapper.java
@@ -8,8 +8,11 @@ public class UserProfileMapper {
 
     public static UserProfileResponse toResponse(UserProfile userProfile){
         UserProfileResponse userProfileResponse = new UserProfileResponse();
+        userProfileResponse.setId(userProfile.getId());
         userProfileResponse.setFirstName(userProfile.getFirstName());
         userProfileResponse.setLastName(userProfile.getLastName());
+        userProfileResponse.setCreatedDate(userProfile.getCreatedDate());
+        userProfileResponse.setModifiedDate(userProfile.getModifiedDate());
         return userProfileResponse;
     }
 }
