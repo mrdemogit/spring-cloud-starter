6af1d5f524c1920c0c3a75608e19bdf98eddb5b9
Maros Demko <maros.demko.it@gmail.com>
2018-12-04 22:53:41 +0800

    [FEATURE] Add Consul service discovery

--- service/example-service/src/main/java/com/example/Application.java
+++ service/example-service/src/main/java/com/example/Application.java
@@ -3,14 +3,20 @@ package com.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
+import javax.sql.DataSource;
 import java.util.concurrent.Executors;
 
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
@@ -20,6 +26,16 @@ public class Application {
     @Bean
     public Scheduler jdbcScheduler() {
         return Schedulers.fromExecutor(Executors.newFixedThreadPool(connectionPoolSize));
+    }
+
+    @Bean
+    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+        return new TransactionTemplate(transactionManager);
+    }
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().build();
     }
 
 
