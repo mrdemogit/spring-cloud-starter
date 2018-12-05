725f4ced36d22692e334d5f37043783522dafebe
Maros Demko <maros.demko.it@gmail.com>
2018-12-05 21:16:41 +0800

    [Feature] Shared config (#4)
    * [FEATURE] Add Consul service discovery
    
    * [SERVICE] Shared config with Consul

---
+++ service/example-service/src/main/java/com/example/config/SharedProperties.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+@ConfigurationProperties("shared")
+@Getter
+@Setter
+public class SharedProperties {
+
+    private String attribute;
+}
